3e1ac36a16e27367b0f720b88bb66daa8ff77960
Tomachi <8929706+book000@users.noreply.github.com>
2023-01-29 14:03:33 +0900

    ci: reusable workflow ci (#142)
    * ci: reusable workflow ci
    
    * fix: ncc & papermcの依存ファイル追加
    
    * fix: entrypoint path
    
    * fix: mvn version

--- papermc/entrypoint.sh
+++ papermc/entrypoint.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 urlPrefix=https://papermc.io/api/v2/projects/paper
-if [ "${MC_VERSION}" = "latest" ]; then
+if [[ "${MC_VERSION}" = "latest" ]]; then
     # Get the latest MC version
     MC_VERSION=$(wget -qO - $urlPrefix | jq -r '.versions[-1]')
 fi
 urlPrefix=${urlPrefix}/versions/${MC_VERSION}
-if [ "${PAPER_BUILD}" = "latest" ]; then
+if [[ "${PAPER_BUILD}" = "latest" ]]; then
       # Get the latest build
       PAPER_BUILD=$(wget -qO - "$urlPrefix" | jq '.builds[-1]')
 fi
 
 JAR_NAME=papermc-${MC_VERSION}-${PAPER_BUILD}.jar
 
-if [ ! -e "${JAR_NAME}" ]
+if [[ ! -e "${JAR_NAME}" ]]
   then
     rm -f ./*.jar
     wget -q "${urlPrefix}/builds/${PAPER_BUILD}/downloads/paper-${MC_VERSION}-${PAPER_BUILD}.jar" -O "$JAR_NAME"
-    if [ ! -e eula.txt ]
+    if [[ ! -e eula.txt ]]
     then
       java -jar "${JAR_NAME}"
       sed -i 's/false/true/g' eula.txt
@@ -25,5 +25,5 @@ if [ ! -e "${JAR_NAME}" ]
 fi
 
 # Start server
-# shellcheck disable=SC2086
+# shellcheck disable=SC2086,SC2154
 exec "java" "-server" "-Xms${MC_RAM}" "-Xmx${MC_RAM}" ${JAVA_OPTS} "-jar" "${JAR_NAME}" "nogui"
